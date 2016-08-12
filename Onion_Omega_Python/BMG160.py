d2746163f886168f1f448c80a751c80366b53dc9
Yadwinder SIngh <apple@Yaddis-iMac.local>
2016-08-13 01:52:57 +0530

    Onion Omega Python Commit

---
+++ Onion_Omega_Python/BMG160.py
@@ -0,0 +1,45 @@
+# Distributed with a free-will license.
+# Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+# BMG160
+# This code is designed to work with the BMG160_I2CS I2C Mini Module available from ControlEverything.com.
+# https://www.controleverything.com/content/Gyro?sku=BMG160_I2CS#tabs-0-product_tabset-2
+
+from OmegaExpansion import onionI2C
+import time
+
+# Get I2C bus
+i2c = onionI2C.OnionI2C()
+
+# BMG160 address, 0x68(104)
+# Select range register, 0x0F(15)
+#		0x80(128)	Configure full scale range = 2000dps
+i2c.writeByte(0x68, 0x0F, 0x80)
+# BMG160 address, 0x68(104)
+# Select bandwidth register, 0x10(16)
+#		0x04(04)	Set bandwidth = 200Hz
+i2c.writeByte(0x68, 0x10, 0x04)
+
+time.sleep(0.5)
+
+# BMG160 address, 0x68(104)
+# Read data back from 0x02(02), 6 bytes
+# X-Axis LSB, X-Axis MSB, Y-Axis LSB, Y-Axis MSB, Z-Axis LSB, Z-Axis MSB
+data = i2c.readsBytes(0x68, 0x02, 6)
+
+# Convert the data
+xGyro = data[1] * 256 + data[0]
+if xGyro > 32767 :
+    xGyro -= 65536
+
+yGyro = data[3] * 256 + data[2]
+if yGyro > 32767 :
+	yGyro -= 65536
+
+zGyro = data[5] * 256 + data[4]
+if zGyro > 32767 :
+	zGyro -= 65536
+
+# Output data to screen
+print "X-Axis of Rotation : %d" %xGyro
+print "Y-Axis of Rotation : %d" %yGyro
+print "Z-Axis of Rotation : %d" %zGyro
